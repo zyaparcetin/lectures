38d628b870af27ed11283a84537e4cb1df41e4c7
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-11-20 15:09:53 +0000

    Revise get and set functions

--- homework/src/index.js
+++ homework/src/index.js
@@ -8,100 +8,92 @@
   - The end date should always be at least 1 day after today.
 */
 
-
 const dayInMs = 1000 * 60 * 60 * 24
 const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate())
 
-
 class Reservation {
   #startDate
+
   #endDate
 
-
   get duration() {
-    return (this.#endDate - this.#startDate) / dayInMs
+    return (this.endDate - this.startDate) / dayInMs
+  }
+
+  get startDate() {
+    return this.#startDate
   }
 
   set startDate(newDate) {
     if (newDate >= today) {
       this.#startDate = newDate
     } else {
-      throw new Error('start date should be today or in the future')
+      throw new Error('The start date should always be today or in the future.')
+    }
   }
-}
+
+  get endDate() {
+    return this.#endDate
+  }
 
   set endDate(newDate) {
     if (newDate > this.#startDate) {
       this.#endDate = newDate
+    } else if (newDate === today) {
+      throw new Error('The end date should always be at least 1 day after today.')
     } else {
-      throw new Error('end date should be later than start date');  
+      throw new Error('The end date should always be at least 1 day after the start date.')
     }
   }
 }
 
-
-/*function shouldPrintDuration2() {
+function shouldPrintDuration2() {
   const reservation = new Reservation()
 
-
-  reservation.startDate = new Date('2022-11-18')
-  reservation.endDate = new Date('2022-11-20')
-
+  reservation.startDate = new Date('2022-11-20')
+  reservation.endDate = new Date('2022-11-22')
 
   console.log(reservation.duration)
-}*/
-
+}
 
 function shouldThrowErrorEndDateIsSmaller() {
   const reservation = new Reservation()
 
-
-  reservation.startDate = new Date('2022-11-18')
+  reservation.startDate = new Date('2022-11-20')
   reservation.endDate = new Date('2022-11-05')
 
-
   console.log(reservation.duration)
 }
 
-
-/*function shouldThrowErrorStartDateInThePast() {
+function shouldThrowErrorStartDateInThePast() {
   const reservation = new Reservation()
 
-
-  reservation.startDate = new Date('2022-11-05')
-  reservation.endDate = new Date('2022-11-18')
-
+  reservation.startDate = new Date('2022-11-19')
+  reservation.endDate = new Date('2022-11-25')
 
   console.log(reservation.duration)
 }
-
 
 function shouldThrowErrorEndDateIsToday() {
   const reservation = new Reservation()
 
-
-  reservation.startDate = new Date('2022-11-18')
-  reservation.endDate = new Date('2022-11-18')
-
+  reservation.startDate = new Date('2022-11-20')
+  reservation.endDate = new Date('2022-11-20')
 
   console.log(reservation.duration)
 }
-
 
 function shouldThrowErrorEndDateIsSmallerThanToday() {
   const reservation = new Reservation()
 
-
-  reservation.startDate = new Date('2022-11-18')
+  reservation.startDate = new Date('2022-11-20')
   reservation.endDate = new Date('2022-11-17')
-
 
   console.log(reservation.duration)
 }
-*/
 
-shouldPrintDuration2()
-shouldThrowErrorEndDateIsSmaller()
-shouldThrowErrorStartDateInThePast()
-shouldThrowErrorEndDateIsToday()
+// shouldPrintDuration2()
+// shouldThrowErrorEndDateIsSmaller()
+// shouldThrowErrorStartDateInThePast()
+// shouldThrowErrorEndDateIsToday()
 shouldThrowErrorEndDateIsSmallerThanToday()
---
+++ new/README.md
@@ -0,0 +1,105 @@
+# Coyotiv - School of Software Engineering
+  ## NODE + PLANTUML starter Template  
+&nbsp;
+
+---
+
+&nbsp; 
+ 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+&nbsp;  
+
+[![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/coyotiv/sose-node)
+&nbsp;  
+
+Now you can directly work in your browser!  
+&nbsp;
+
+## Getting started  
+&nbsp;
+- ### PLANTUML
+  &nbsp;
+  > **Note:**  
+    > - There is a **class.puml** example file located in the **_architecture_** folder.  
+  &nbsp;
+  <p>&nbsp;</p>
+
+  To start working **_*.puml_** files:  
+  
+  - You can continue by editing the **class.puml** file   
+  - Add additional **_*.puml_** files on that folder too.  
+  &nbsp;  
+
+  If you what to visualize a preview of the **class.puml** file or another **_*.puml_** file on the project, please select the file and on your keyboard press the following keys:
+  <p>&nbsp;</p>  
+
+  - _MacOS_: **Option + D**  
+  
+  - _Linux/Windows_: **Alt + D**  
+  <p>&nbsp;</p>
+  The above commands will automatically open your a window and will show you live any updates that you make on the file.    
+  <p>&nbsp;</p> 
+
+- ### NODE / JS  
+  &nbsp; 
+  > **Note:**
+    >-  _There is an **index.js** file located under the **_src_** folder_  
+    >-  _The **index.js** file, it's already linked with the debugger in your **launch.json**_  
+  &nbsp;   
+  <p>&nbsp;</p>  
+
+  To run the **index.js** file:
+    <p>&nbsp;</p> 
+
+    - with **_node_**, copy and enter one of the following commands on your terminal:
+
+      ```sh
+      npm start
+      ```
+      or:
+
+      ```sh
+      node src/index.js
+      ```
+    <p>&nbsp;</p>  
+
+    - with **_nodemon_**, copy and enter one of the following commands on your terminal:
+
+      ```sh
+      npm run dev
+      ```
+      or:
+
+      ```sh
+      nodemon src/index.js
+      ```
+  &nbsp;
+---  
+ &nbsp;  
+_Now that we are ready, let's have some coding fun!_
+
+---
+
+&nbsp;
+## MIT License
+
+Copyright (c) 2022 Coyotiv
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
